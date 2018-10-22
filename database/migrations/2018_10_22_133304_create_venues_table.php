e7086e79f21f95ceccff10bc948283787aca535c
trave84 <markerterheaven@gmail.com>
2018-10-22 17:09:57 +0200

    MIgration and models added

--- database/migrations/2018_10_22_133304_create_venues_table.php
+++ database/migrations/2018_10_22_133304_create_venues_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateVenuesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('venues', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('location');
+            $table->string('budget');
+            $table->string('opening_hours');
+            $table->string('cuisine');
+            $table->string('venue_type');
+            $table->string('links');
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('venues');
     }
 }
